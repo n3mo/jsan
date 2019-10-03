68ac16ca96546c0bd1d86d321f797d6f337bd2ad
nemo <nemo1211@gmail.com>
2019-10-03 19:56:51 -0400

    Indexing nested JSON fields now possible

--- src/main.rs
+++ src/main.rs
@@ -5,79 +5,6 @@ use std::fs::File;
 use serde_json::Value;
 use clap::{Arg, App};
 
-// fn main() -> std::io::Result<()> {
-
-//     // Command line parsing with clap
-//     let args = App::new("jsan -- The JSON Swiss Army kNife")
-//         .version("0.0.1")
-//         .author("Nicholas M. Van Horn (nvanhorn@protonmail.com)")
-//         .about("A fast JSON scraping tool")
-//         .arg(Arg::with_name("field")
-//              .short("f")
-//              .long("field")
-//              .help("Desired JSON fields to scrape")
-//              .required(true)
-//              .takes_value(true))
-//         .arg(Arg::with_name("input")
-//              .short("i")
-//              .long("input")
-//              .help("Sets the input file to use")
-//              .takes_value(true))
-//         .get_matches();
-
-//     // Command line arguments
-//     //let args: Vec<String> = env::args().collect();
-
-//     // The first arg is the name of the program, so we'll
-//     // skip to the second argument for the file name
-//     // let input_file = &args[1];
-//     // let json_fields = &args[2..];
-//     // let num_fields = json_fields.len();
-
-//     let input_file: &str = match args.is_present("input") {
-//         true => args.value_of("input").unwrap(),
-//         false => args.value_of("input").unwrap(),
-//     };
-
-    
-//     // let json_fields = &args[2..];
-//     // let num_fields = json_fields.len();
-//     let json_fields: Vec<&str> = args.values_of("field").unwrap().collect();
-//     let num_fields = json_fields.len();
-    
-//     // let f = File::open("data/tweets.json")?;
-//     let f = File::open(input_file)?;
-//     let reader = BufReader::new(f);
-//     let mut writer = BufWriter::new(io::stdout());
-
-//     // Work across the file line-by-line, using buffered reading
-//     // and writing
-//     for line in reader.lines() {
-//         match line {
-//             Ok(line) => {
-//                 // Parse the current line's JSON string
-//                 let v: Value = serde_json::from_str(&line)?;
-
-//                 // Parse and write each requested JSON field
-//                 // for field in json_fields {
-//                 for i in 0..num_fields {
-//                     writer.write(v[&json_fields[i]].to_string().as_bytes())
-//                         .expect("Error: Malformed JSON?");
-//                     if i < num_fields - 1 {
-//                         writer.write(b",").unwrap();
-//                     }
-//                 }
-//                 writer.write(b"\n").unwrap();
-//             },
-//             Err(e) => {
-//                 println!("Err: {}", e);
-//             },
-//         }
-//     }
-
-
-//     Ok(())
-// }
 fn main() -> std::io::Result<()> {
 
     // Command line parsing with clap
@@ -90,6 +17,7 @@ fn main() -> std::io::Result<()> {
              .long("field")
              .help("Desired JSON fields to scrape")
              .required(true)
+             .min_values(1)
              .takes_value(true))
         .arg(Arg::with_name("input")
              .short("i")
@@ -122,7 +50,16 @@ fn main() -> std::io::Result<()> {
                 // Parse and write each requested JSON field
                 // for field in json_fields {
                 for i in 0..num_fields {
-                    writer.write(v[&json_fields[i]].to_string().as_bytes())
+
+                    let mut child = v.clone();
+                    let mut child2 = child.clone();
+                    
+                    for idx in json_fields[i].split("::") {
+                        child2.clone_from(&child[idx]);
+                        child.clone_from(&child2);
+                    }
+
+                    writer.write(child.to_string().as_bytes())
                         .expect("Error: Malformed JSON?");
                     if i < num_fields - 1 {
                         writer.write(b",").unwrap();
@@ -136,6 +73,8 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-
     Ok(())
 }
+
+
+
